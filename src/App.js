d40b473d741e929835405ae8e871fd8229c4c1a0
AnthonyLzq <sluzquinosa@uni.pe>
2022-03-17 21:20:05 -0500

    feat: implemented messaging api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import { useEffect } from "react";
 import app from "firebase/app";
-import functions from "firebase-functions"
+import "firebase/messaging";
 
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   useEffect(() => {
+    const config = {
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+      databaseURL: process.env.REACT_APP_DATABASE_URL,
+      projectId: process.env.REACT_APP_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    };
+    app.initializeApp(config);
+    const messaging = app.messaging();
+    messaging
+      .requestPermission()
+      .then(() => {
+        console.log('messaging.getToken', messaging.getToken());
+      })
+
     console.log('app', app)
-    console.log('functions', functions)
   }, [])
 
   return (
